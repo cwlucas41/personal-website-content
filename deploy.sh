b2fd274810ec44c11f9cf33adfd98a9cc63578ea
Chris Lucas <chris@chriswlucas.com>
2019-06-27 02:27:24 -0700

    adds invalidation to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 aws s3 cp . s3://chriswlucas.com-origin/ --recursive --exclude ".*" --exclude "deploy.sh"
+aws cloudfront create-invalidation --distribution-id E3L2VRGKVCQNT0 --paths '/*'
